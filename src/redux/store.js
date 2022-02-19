bc66ad8390dbc8907b9b0cf54c0e9697b985c463
alxdrian <alex_97piscis@outlook.com>
2022-02-19 00:33:28 -0500

    use local storage to persist stor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,32 @@
 import { createStore } from 'redux';
 import reducers from './reducers/index';
 
+function saveToLocalStorage(state) {
+    try {
+      const serialisedState = JSON.stringify(state);
+      localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+      console.warn(e);
+    }
+  }
+
+function loadFromLocalStorage() {
+    try {
+      const serialisedState = localStorage.getItem("persistantState");
+      if (serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    } catch (e) {
+      console.warn(e);
+      return undefined;
+    }
+  }
+
 const store = createStore(
   reducers, 
-  {},
+  loadFromLocalStorage(),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
   
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
 export default store;
